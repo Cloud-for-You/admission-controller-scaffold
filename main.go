586b954f3529de1166896cb687388b47ac22135d
Petr Haton <petr.haton@unicov-net.com>
2023-10-11 09:31:37 +0200

    Prejmenovani adresare

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ import (
 
 	//+kubebuilder:scaffold:imports
 
-	webhookadmission "github.com/Cloud-for-You/admission-controller-scaffold/admission"
+	webhookadmission "github.com/Cloud-for-You/admission-controller-scaffold/admissions"
 )
 
 var (
