0178057f89e1c3a3e12b5b45885874e7b147a4ca
Petr Haton <petr.haton@unicov-net.com>
2023-10-12 09:10:25 +0200

    Pridani logovani do admission controlleru

--- admissions/sample.go
+++ admissions/sample.go
@@ -6,6 +6,12 @@ import (
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+var (
+	logger = ctrl.Log.WithName("sample-admission")
 )
 
 type Sample struct {
@@ -14,6 +20,8 @@ type Sample struct {
 }
 
 func (v *Sample) Handle(ctx context.Context, req admission.Request) admission.Response {
+	logger.Info("Handling admission webhook ...")
+
 	// Return func for DENY
 	return admission.Denied(fmt.Sprintf("Missing one or more of minimum required parameters. severity: %v, example_response_code: %v, example_content: %v", "DEBUG", "Response Code", "Content"))
 	// Return func for ALLOW
